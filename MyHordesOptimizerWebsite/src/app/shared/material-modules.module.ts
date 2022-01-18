ba4cd723ea2fb9651dba8ac029d8d68ccfd42b41
Hélène Godart <lenoune38@gmail.com>
2022-01-18 01:01:44 +0100

    Website
    amélioration visuelle

--- MyHordesOptimizerWebsite/src/app/shared/material-modules.module.ts
+++ MyHordesOptimizerWebsite/src/app/shared/material-modules.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -13,7 +14,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 let material_modules: any[] = [
-    MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatProgressSpinnerModule, MatSelectModule,
+    MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatProgressSpinnerModule, MatSelectModule,
     MatSidenavModule, MatSnackBarModule, MatTableModule, MatToolbarModule
 ];
 
