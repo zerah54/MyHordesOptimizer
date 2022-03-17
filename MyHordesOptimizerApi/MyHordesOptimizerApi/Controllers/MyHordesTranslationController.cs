62a036aabc9530db4f54f4df04326034073a4597
ReNacK <pierre.chiriot@telecomnancy.net>
2022-03-17 21:35:38 +0100

    Ajout du endpoint de translation

---
+++ MyHordesOptimizerApi/MyHordesOptimizerApi/Controllers/MyHordesTranslationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyHordesOptimizerApi.Controllers.Abstract;
+using MyHordesOptimizerApi.Dtos.MyHordesOptimizer.Translations;
+using MyHordesOptimizerApi.Providers.Interfaces;
+using MyHordesOptimizerApi.Services.Interfaces.Translations;
+
+namespace MyHordesOptimizerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyHordesTranslationController : AbstractMyHordesOptimizerControllerBase
+    {
+        protected ITranslationService TranslationService { get; set; }
+        public MyHordesTranslationController(ILogger<AbstractMyHordesOptimizerControllerBase> logger, 
+            IUserInfoProvider userKeyProvider,
+            ITranslationService translationService) : base(logger, userKeyProvider)
+        {
+            TranslationService = translationService;
+        }
+
+        [HttpGet]
+        public ActionResult<TranslationResultDto> GetTranslation(string locale, string sourceString)
+        {
+            if (string.IsNullOrWhiteSpace(locale))
+            {
+                return BadRequest($"{nameof(locale)} cannot be empty");
+            }
+            if (string.IsNullOrWhiteSpace(sourceString))
+            {
+                return BadRequest($"{nameof(sourceString)} cannot be empty");
+            }
+            var translationResult = TranslationService.GetTranslation(locale, sourceString);
+            return translationResult;
+        }
+    }
+}
