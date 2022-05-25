b0e26ae6676cca842056bbf943060d8b2b70d0f4
Hélène Godart <lenoune38@gmail.com>
2022-05-25 18:30:21 +0200

    create utils.js

---
+++ Scripts/Tampermonkey/my_hordes_optimizer/utils.js
@@ -0,0 +1,106 @@
+/////////////////////////////////////////
+// Fonctions utiles / Useful functions //
+/////////////////////////////////////////
+
+/** @return {string}     website language */
+function getWebsiteLanguage() {
+    return document.getElementsByTagName('html')[0].attributes.lang.value;
+}
+
+/** @return {boolean}     true if button exists */
+function buttonOptimizerExists() {
+    return document.getElementById(btn_id);
+}
+
+/** @return {boolean}    true si la page de l'utilisateur est la page de la ville */
+function pageIsTown() {
+    return document.URL.indexOf('town') > -1;
+}
+
+/** @return {boolean}    true si la page de l'utilisateur est la page de l'atelier */
+function pageIsWorkshop() {
+    return document.URL.endsWith('workshop');
+}
+
+
+/** @return {boolean}    true si la page de l'utilisateur est la page des chantiers */
+function pageIsConstructions() {
+    return document.URL.endsWith('constructions');
+}
+
+/** @return {boolean}    true si la page de l'utilisateur est la page du désert */
+function pageIsDesert() {
+    return document.URL.indexOf('desert') > -1;
+}
+
+/** Affiche ou masque la page de chargement de MyHordes en fonction du nombre d'appels en cours */
+function displayLoading() {
+    let loadzone = document.getElementById('loadzone');
+    if (loading_count > 0) {
+        loadzone.setAttribute('x-stack', 1);
+    } else {
+        loadzone.setAttribute('x-stack', 0);
+    }
+}
+
+/** Affiche la page de chargement de MyHordes */
+function startLoading() {
+    loading_count += 1;
+    displayLoading();
+}
+
+/** Masque la page de chargement de MyHordes */
+function endLoading() {
+    loading_count -= 1;
+    displayLoading();
+}
+
+/** Affiche une notification de réussite */
+function addSuccess(message) {
+    let notifications = document.getElementById('notifications');
+    let notification = document.createElement('div');
+    notification.classList.add('notice', 'show');
+    notification.innerText = `${GM_info.script.name} : ${message}`;
+    notifications.appendChild(notification);
+    notification.addEventListener('click', () => {
+        notification.remove();
+    });
+    setTimeout(() => {
+        notification.remove();
+    }, 5000);
+}
+
+
+/** Affiche une notification de warning */
+function addWarning(message) {
+    let notifications = document.getElementById('notifications');
+    let notification = document.createElement('div');
+    notification.classList.add('warning', 'show');
+    notification.innerText = `${GM_info.script.name} : ${message}`;
+    notifications.appendChild(notification);
+    notification.addEventListener('click', () => {
+        notification.remove();
+    });
+    setTimeout(() => {
+        notification.remove();
+    }, 5000);
+}
+
+/** Affiche une notification d'erreur */
+function addError(error) {
+    let notifications = document.getElementById('notifications');
+    let notification = document.createElement('div');
+    notification.classList.add('error', 'show');
+    notification.innerText = `${GM_info.script.name} : ${api_texts.error[temp_lang].replace('$error$', error.status)}`;
+    if (error.status === 0) {
+        notification.innerText += '\n' + error.error;
+    }
+    notifications.appendChild(notification);
+    notification.addEventListener('click', () => {
+        notification.remove();
+    });
+    setTimeout(() => {
+        notification.remove();
+    }, 5000);
+    console.error(`${GM_info.script.name} : Une erreur s'est produite : \n`, error);
+}
