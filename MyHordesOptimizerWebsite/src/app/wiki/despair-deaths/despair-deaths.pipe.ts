10c46d28aa775b35176e063e619a9f89182b4a2c
Hélène Godart <lenoune38@gmail.com>
2022-11-10 10:48:52 +0100

    despair

---
+++ MyHordesOptimizerWebsite/src/app/wiki/despair-deaths/despair-deaths.pipe.ts
@@ -0,0 +1,13 @@
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+@Pipe({
+    name: 'despairDeaths',
+})
+export class DespairDeathsPipe implements PipeTransform {
+    transform(nb_killed_zombies: number): number {
+        return Math.floor(Math.max(0, (nb_killed_zombies - 1 ) / 2));
+        ;
+    }
+}
