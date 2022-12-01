96c91ecfef346f7a65a30dcff95a8e88c1fc56fe
Hélène Godart <lenoune38@gmail.com>
2022-12-01 10:04:04 +0100

    Update gesthordes_fix_carte_alternative.user.js

--- Scripts/Tampermonkey/gesthordes_fix_carte_alternative.user.js
+++ Scripts/Tampermonkey/gesthordes_fix_carte_alternative.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amélio Gest'Hordes
 // @version      1.1
-// @description  Ajoute les blocs manquants à la carte alternative de GH ainsi que 0 en nombre de zombies quand la case n'a jamais été visitée. Ajoute des boutons pour choisir une direction de regénération du scrutateur sur laquelle appliquer un ajout de marqueurs pelles
+// @description  Ajoute les blocs manquants à la carte alternative de GH ainsi que 0 en nombre de zombies quand la case n'a jamais été visitée. Ajoute des boutons pour choisir une direction de regénération du scrutateur sur laquelle appliquer un ajout de marqueurs pelles. Permet de bouger le bloc des infos de la ville.
 // @author       Zerah
 //
 // @downloadURL  https://github.com/zerah54/MyHordesOptimizer/raw/main/Scripts/Tampermonkey/gesthordes_fix_carte_alternative.user.js
@@ -14,6 +14,8 @@
 // ==/UserScript==
 
 const api = 'https://api.myhordesoptimizer.fr';
+
+let move_info_ville = false;
 
 /** Récupère l'id de session de l'utilisateur */
 async function getPhpSessionId() {
@@ -70,6 +72,7 @@ function addRegenDirectionButtons() {
     scrut_tab.innerText = `+ Direction du scrutateur`
     scrut_tab.classList.add('hidden');
     scrut_tab.style.float = 'right';
+    scrut_tab.style.whiteSpace = 'nowrap';
     scrut_tab.style.cursor = 'pointer';
     scrut_tab.style.width = 'auto';
     scrut_tab.style.padding = '0 0.5em';
@@ -220,6 +223,117 @@ function getCellsContent(click_direction) {
     });
 }
 
+/** Permet de déplacer le bloc info villes */
+function moveBlockInfoVille() {
+    let zoneInfoCarte = document.querySelector('#zoneInfoCarte');
+    if (!move_info_ville) {
+        zoneInfoCarte.classList.remove('movable');
+        zoneInfoCarte.style.width = 'unset';
+        zoneInfoCarte.style.height = 'unset';
+    } else {
+        zoneInfoCarte.classList.add('movable');
+        let move_button = zoneInfoCarte.querySelector('#move_info_carte_button');
+
+        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+        move_button.onmousedown = (e) => {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = () => {
+                document.onmouseup = null;
+                document.onmousemove = null;
+            };
+            // call a function whenever the cursor moves:
+            document.onmousemove = (e) => {
+                e = e || window.event;
+                e.preventDefault();
+                // calculate the new cursor position:
+                pos1 = pos3 - e.clientX;
+                pos2 = pos4 - e.clientY;
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                // set the element's new position:
+                zoneInfoCarte.style.top = (zoneInfoCarte.offsetTop - pos2) + "px";
+                zoneInfoCarte.style.left = (zoneInfoCarte.offsetLeft - pos1) + "px";
+            };
+        }
+    }
+}
+
+function placeMoveButton() {
+    let zone_info_carte = document.querySelector('#zoneInfoCarte');
+    let zone_info_ville_tabs = zone_info_carte.querySelector('ul');
+    let pin_button = document.createElement('span');
+    pin_button.innerHTML = '&#128204;';
+    pin_button.id = 'unpin_info_carte_button'
+    zone_info_ville_tabs.appendChild(pin_button);
+    pin_button.addEventListener('click', (event) => {
+        move_info_ville = !move_info_ville;
+        moveBlockInfoVille();
+    });
+
+    let move_button = document.createElement('span');
+    move_button.innerHTML = '&#10021';
+    move_button.id = 'move_info_carte_button';
+    move_button.style.display = 'none';
+    let first_ul = zone_info_carte.querySelector('ul');
+    first_ul.parentElement.insertBefore(move_button, first_ul);
+
+    const block_page = document.querySelector('#bloc_page');
+    const background_color = window.getComputedStyle(block_page, null).getPropertyValue('background-color');
+    const border_color = window.getComputedStyle(block_page, null).getPropertyValue('border-color');
+
+    const info_ville_style = '#zoneInfoCarte.movable {'
+    + 'position: absolute;'
+    + 'width: 80%;'
+    + 'resize: both;'
+    + 'z-index: 50;'
+    + 'overflow: auto;'
+    + `border: 1px solid ${border_color};`
+    + `background-color: ${background_color};`
+    + '}';
+
+    const ul_style = '#zoneInfoCarte.movable ul {'
+    + 'display: flex;'
+    + 'height: 26px;'
+    + '}';
+
+    const table_style = '#zoneInfoCarte.movable #table_trace {'
+    + 'height: unset;'
+    + '}';
+
+    const move_button_style = '#zoneInfoCarte.movable #move_info_carte_button {'
+    + 'display: initial !important;'
+    + 'float: left;'
+    + 'width: 25px;'
+    + 'text-align: center;'
+    + 'margin: auto;'
+    + 'cursor: move;'
+    + '}';
+
+    const unpin_info_carte_button_style = '#zoneInfoCarte #unpin_info_carte_button {'
+    + 'float: right;'
+    + 'cursor: pointer;'
+    + 'width: auto;'
+    + 'padding: 0 0.5em;'
+    + '}'
+
+    let css = info_ville_style + ul_style + table_style + move_button_style + unpin_info_carte_button_style;
+
+    let style = document.createElement('style');
+
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+
+    document.getElementsByTagName('head')[0].appendChild(style);
+}
+
 (function() {
     'use strict';
 
@@ -269,5 +383,6 @@ function getCellsContent(click_direction) {
         });
     }
 
+    placeMoveButton();
     addRegenDirectionButtons();
 })();
