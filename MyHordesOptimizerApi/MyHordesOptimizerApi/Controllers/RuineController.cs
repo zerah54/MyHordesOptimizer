ca6dacbe1656a24bbce1b35f618f89388d08c2dc
ReNacK <pierre.chiriot@telecomnancy.net>
2022-12-17 22:18:14 +0100

    Gestion du attaqueestimation controller propre

--- MyHordesOptimizerApi/MyHordesOptimizerApi/Controllers/RuineController.cs
+++ MyHordesOptimizerApi/MyHordesOptimizerApi/Controllers/RuineController.cs
@@ -33,52 +33,6 @@ namespace MyHordesOptimizerApi.Controllers
         }
 
         [HttpPost]
-        [Route("Planif")]
-        public ActionResult<string> Planif()
-        {
-            var results = new List<string>();
-            var redSouls = 0;
-            //var min = 3275;
-            //var max = 5400;
-            //var min = 3406;
-            //var min = 14000;
-            //var max = 22225;
-            var min = 4975;
-            var max = 8725;
-            var day = 23;
-            var dayAttaque = day + 1;
-            var max_ratio = 1;
-            var ratio_min = dayAttaque <= 3 ? 0.66 : max_ratio;
-            var ratio_max = dayAttaque <= 3 ? (dayAttaque <= 1 ? 0.4 : 0.66) : max_ratio;
-            var attaqueMin = Math.Round(ratio_min * Math.Pow(Math.Max(1, dayAttaque - 1) * 0.75 + 2.5, 3));
-            var attaqueMax = Math.Round(ratio_max * Math.Pow(dayAttaque * 0.75 + 3.5, 3));
-            for (var attaque = attaqueMin; attaque <= attaqueMax; attaque++)
-            {
-                for (var k = 15; k <= 36; k++)
-                {
-                    var offsetMin = k;
-                    var offsetMax = 48 - offsetMin;
-                    var min2 = Math.Round(attaque - (attaque * offsetMin / 100));
-                    var max2 = Math.Round(attaque + (attaque * offsetMax / 100));
-
-                    min2 = Math.Floor(min2 / 25) * 25;
-                    max2 = Math.Ceiling(max2 / 25) * 25;
-
-                    var soulFactor = Math.Min(1 + (0.04 * redSouls), 1.2);
-
-                    min2 = Math.Round(min2 * soulFactor);
-                    max2 = Math.Round(max2 * soulFactor);
-
-                    if (min == min2 && max == max2)
-                    {
-                        results.Add($"{min} - {max} : {attaque}");
-                    }
-                }
-            }
-            return Ok(results);
-        }
-
-        [HttpPost]
         [Route("Attaque")]
         public ActionResult<string> Attaque([FromBody] AttaqueInputDto input)
         {
---
+++ MyHordesOptimizerApi/MyHordesOptimizerApi/Dtos/MyHordesOptimizer/AttaqueEstimationRequest.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace MyHordesOptimizerApi.Dtos.MyHordesOptimizer
+{
+    public class AttaqueEstimationRequest
+    {
+        [JsonProperty("day")]
+        public int Day { get; set; }
+
+        [JsonProperty("min")]
+        public int Min { get; set; }
+
+        [JsonProperty("max")]
+        public int Max { get; set; }  
+
+        [JsonProperty("redSouls")]
+        public int RedSouls { get; set; }
+    }
+}
