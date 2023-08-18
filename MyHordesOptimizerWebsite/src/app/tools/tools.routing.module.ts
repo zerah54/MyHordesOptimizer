e5193ecbd1787c910b1fbbc5227da5957cdbbaf7
Hélène Godart <lenoune38@gmail.com>
2023-08-18 12:49:16 +0200

    ajout de la lecture du registre pour ajouter des fouilles

--- MyHordesOptimizerWebsite/src/app/tools/tools.routing.module.ts
+++ MyHordesOptimizerWebsite/src/app/tools/tools.routing.module.ts
@@ -4,15 +4,15 @@ import { CampingComponent } from './camping/camping.component';
 import { ProbabilitiesComponent } from './probabilities/probabilities.component';
 
 const routes: Routes = [
-    {path: '', redirectTo: 'tools/bank', pathMatch: 'full'},
-    {path: 'tools', redirectTo: 'tools/bank'},
+    { path: '', redirectTo: 'tools/bank', pathMatch: 'full' },
+    { path: 'tools', redirectTo: 'tools/bank' },
     {
         path: 'tools', children: [
-            {path: 'camping', component: CampingComponent, title: 'MyHordes Optimizer' + ' - ' + $localize`Outils` + ' - ' + $localize`Camping`},
+            { path: 'camping', component: CampingComponent, title: 'MyHordes Optimizer' + ' - ' + $localize`Outils` + ' - ' + $localize`Camping` },
             {
                 path: 'probabilities',
                 component: ProbabilitiesComponent,
-                title: 'MyHordes Optimizer' + ' - ' + $localize`Outils` + ' - ' + $localize`Probabilités`
+                title: 'MyHordes Optimizer' + ' - ' + $localize`Outils` + ' - ' + $localize`Chances de survie`
             },
             // { path: 'estimation', component: EstimationComponent, title: 'MyHordes Optimizer' + ' - ' + $localize`Outils` + ' - ' + $localize`Estimations` },
         ]
