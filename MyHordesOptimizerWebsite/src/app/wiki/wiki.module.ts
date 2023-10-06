3a421093bb4b43dffd590d32abea9d45bac39208
Hélène Godart <lenoune38@gmail.com>
2023-10-07 01:57:42 +0200

    [new] Calculateur de zombies tués par désespoir

--- MyHordesOptimizerWebsite/src/app/wiki/wiki.module.ts
+++ MyHordesOptimizerWebsite/src/app/wiki/wiki.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
-import { SharedModule } from '../shared/shared.module';
 import { Components } from '../_abstract_model/types/_types';
+import { SharedModule } from '../shared/shared.module';
 import { HeroSkillsComponent } from './hero-skills/hero-skills.component';
 import { ItemsComponent } from './items/items.component';
+import { DespairDeathsCalculatorComponent } from './miscellaneous-info/despair-deaths-calculator/despair-deaths-calculator.component';
 import { MiscellaneousInfoComponent } from './miscellaneous-info/miscellaneous-info.component';
 import { DespairDeathsPipe } from './miscellaneous-info/miscellaneous-info.pipe';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RuinsComponent } from './ruins/ruins.component';
 import { WikiRoutingModule } from './wiki.routing.module';
 
-const components: Components = [HeroSkillsComponent, ItemsComponent, RecipesComponent, RuinsComponent, MiscellaneousInfoComponent];
+const components: Components = [
+    HeroSkillsComponent, ItemsComponent, RecipesComponent, RuinsComponent, MiscellaneousInfoComponent, DespairDeathsCalculatorComponent
+];
 const local_components: Components = [DespairDeathsPipe];
 
 @NgModule({
