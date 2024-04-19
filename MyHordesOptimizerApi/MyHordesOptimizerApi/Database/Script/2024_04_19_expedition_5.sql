9812e50aafedd23136516c39fc4063764243020f
ReNacK <pierre.chiriot@telecomnancy.net>
2024-04-19 23:58:07 +0200

    fix type isPreinscritSoif

--- MyHordesOptimizerApi/MyHordesOptimizerApi/Database/Script/2024_04_19_expedition_5.sql
+++ MyHordesOptimizerApi/MyHordesOptimizerApi/Database/Script/2024_04_19_expedition_5.sql
@@ -2,5 +2,8 @@
 MODIFY nombrePaDepart int NULL NULL;
 
 ALTER TABLE ExpeditionCitizen
-ADD COLUMN isPreinscritSoif int NULL NULL
+ADD COLUMN isPreinscritSoif INT NULL NULL
 AFTER isThirsty;
+
+ALTER TABLE ExpeditionCitizen
+MODIFY isPreinscritSoif BIT NULL NULL;
