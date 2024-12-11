728a4f9d72c3bef1b9a966ceafe7f85bdf39df0e
Hélène Godart <lenoune38@gmail.com>
2024-12-11 22:34:51 +0100

    fix registre

--- MyHordesOptimizerWebsite/src/app/my-town/statistics/registry/well/well-registry.component.ts
+++ MyHordesOptimizerWebsite/src/app/my-town/statistics/registry/well/well-registry.component.ts
@@ -23,10 +23,10 @@ const material_modules: Imports = [MatDividerModule];
 export class WellRegistryComponent {
     @HostBinding('style.display') display: string = 'contents';
 
-    @Input({ required: true }) completeCitizenList!: CitizenInfo;
-    @Input({ required: true }) displayPseudo!: DisplayPseudoMode;
+    @Input({required: true}) completeCitizenList!: CitizenInfo;
+    @Input({required: true}) displayPseudo!: DisplayPseudoMode;
 
-    @Input({ required: true }) set registry(registry: Entry[] | undefined) {
+    @Input({required: true}) set registry(registry: Entry[] | undefined) {
         if (registry) {
             this.entries = registry.filter((entry: Entry) => {
                 return this.well_keywords.some((well_keywords: string): boolean => entry.entry?.indexOf(' ' + well_keywords + ' ') > -1);
@@ -40,7 +40,7 @@ export class WellRegistryComponent {
 
     private readonly well_keywords: string[] = [
         'a pris une ration', 'hat eine Ration Wasser genommen', 'has taken a ration', 'ha tomado una ración',
-        's\'est permis de prendre plus d\'eau', 'hat mehr Wasser genommen', 'has decided it was ok for them to take more water', 'se ha dado el lujo de tomar más agua',
+        'permis de prendre plus', 'hat mehr Wasser genommen', 'has decided it was ok for them to take more water', 'se ha dado el lujo de tomar más agua',
     ];
 
 }
