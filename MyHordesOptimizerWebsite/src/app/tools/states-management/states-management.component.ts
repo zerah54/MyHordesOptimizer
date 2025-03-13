257ca84a6f446d9b0555c98e011482821a49b916
Yanis <yanistomo@hotmail.fr>
2025-03-13 22:56:02 +0100

    UNACCEPTABLE

--- MyHordesOptimizerWebsite/src/app/tools/states-management/states-management.component.ts
+++ MyHordesOptimizerWebsite/src/app/tools/states-management/states-management.component.ts
@@ -1,18 +1,22 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggle, MatButtonToggleGroup } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatList, MatListItem } from '@angular/material/list';
+import { MatList, MatListItem, MatListItemLine } from '@angular/material/list';
 import { MatOption, MatSelect } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import moment from 'moment';
+import { HORDES_IMG_REPO } from '../../_abstract_model/const';
 import { Imports } from '../../_abstract_model/types/_types';
-import { SelectComponent } from '../../shared/elements/select/select.component';
-import { FilterRuinsByKmPipe } from '../../shared/pipes/filter-ruins-by-km.pipe';
+import { IconApComponent } from '../../shared/elements/icon-ap/icon-ap.component';
+import { IconEpComponent } from '../../shared/elements/icon-ep/icon-ep.component';
+import { CampingDisplayBonusPipe } from '../camping/camping-display-bonus.pipe';
 
 const angular_common: Imports = [CommonModule, FormsModule];
 const components: Imports = [];
@@ -23,7 +27,8 @@ const material_modules: Imports = [MatButtonModule, MatCardModule, MatFormFieldM
     selector: 'mho-thirst-management',
     templateUrl: './states-management.component.html',
     styleUrls: ['./states-management.component.scss'],
-    imports: [...angular_common, ...components, ...material_modules, ...pipes, SelectComponent, FilterRuinsByKmPipe, MatSelect, MatOption, NgOptimizedImage, MatList, MatListItem]
+    imports: [...angular_common, ...components, ...material_modules, ...pipes, MatSelect, MatOption, NgOptimizedImage, MatList, MatListItem, IconApComponent, MatDivider, MatListItemLine, MatButtonToggleGroup, MatButtonToggle, CampingDisplayBonusPipe, IconEpComponent],
+    encapsulation:ViewEncapsulation.None
 })
 export class StatesManagementComponent {
     @HostBinding('style.display') display: string = 'contents';
@@ -35,9 +40,9 @@ export class StatesManagementComponent {
     ]
 
     actions: actions[] = [
-        {value: 'water', viewValue: "Prise de gourde"},
-        {value: 'b6', viewValue: "Nourriture (B6)"},
-        {value: 'move', viewValue: "Déplacement dans l'OM"},
+        {value: 'water', viewValue: 'Prise de gourde'},
+        {value: 'b6', viewValue: 'Nourriture (B6)'},
+        {value: 'move', viewValue: 'Déplacement dans l\'OM'},
     ];
 
     public addAction(): void {
@@ -45,6 +50,19 @@ export class StatesManagementComponent {
         let states = this.states_and_actions.filter((state: StateOrAction) => state.type === 'state')
         this.states_and_actions.push({...states[states.length - 1]});
     }
+
+    toggleHydratedStatus($event: MouseEvent, value: string, buttonToggle: MatButtonToggle) {
+        console.log(value);
+        console.log(buttonToggle);
+        if(buttonToggle.checked){
+            $event.stopPropagation();
+            $event.preventDefault();
+            //buttonToggle.value = undefined;
+            buttonToggle.checked = false;
+        }
+    }
+
+    protected readonly HORDES_IMG_REPO = HORDES_IMG_REPO;
 }
 
 interface actions {
