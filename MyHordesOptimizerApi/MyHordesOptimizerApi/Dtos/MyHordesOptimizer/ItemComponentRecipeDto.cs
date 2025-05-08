6bc1576432f8312a83aebd38f51e97238956c9b8
ReNacK <pierre.chiriot@telecomnancy.net>
2025-05-09 00:50:33 +0200

    fix recette avec items en doublon: On rajoute le count

---
+++ MyHordesOptimizerApi/MyHordesOptimizerApi/Dtos/MyHordesOptimizer/ItemComponentRecipeDto.cs
@@ -0,0 +1,10 @@
+﻿using MyHordesOptimizerApi.Dtos.MyHordes.MyHordesOptimizer;
+
+namespace MyHordesOptimizerApi.Dtos.MyHordesOptimizer
+{
+    public class ItemComponentRecipeDto
+    {
+        public ItemWithoutRecipeDto Item { get; set; }
+        public int Count { get; set; }
+    }
+}
--- MyHordesOptimizerApi/MyHordesOptimizerApi/Dtos/MyHordesOptimizer/ItemRecipeDto.cs
+++ MyHordesOptimizerApi/MyHordesOptimizerApi/Dtos/MyHordesOptimizer/ItemRecipeDto.cs
@@ -8,13 +8,13 @@ namespace MyHordesOptimizerApi.Dtos.MyHordesOptimizer
         public string Name { get; set; }
         public string Type { get; set; }
         public List<ItemResultDto> Result { get; set; }
-        public List<ItemWithoutRecipeDto> Components { get; set; }
+        public List<ItemComponentRecipeDto> Components { get; set; }
         public IDictionary<string, string> Actions { get; set; }
 
         public ItemRecipeDto()
         {
             Actions = new Dictionary<string, string>();
-            Components = new List<ItemWithoutRecipeDto>();
+            Components = new List<ItemComponentRecipeDto>();
             Result = new List<ItemResultDto>();
         }
     }
