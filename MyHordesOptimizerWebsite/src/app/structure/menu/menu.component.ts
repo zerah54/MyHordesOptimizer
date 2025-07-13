08dc043b692977a6929b692b609e1a795c33bc32
Hélène Godart <lenoune38@gmail.com>
2025-07-13 02:29:09 +0200

    Merge

--- MyHordesOptimizerWebsite/src/app/structure/menu/menu.component.ts
+++ MyHordesOptimizerWebsite/src/app/structure/menu/menu.component.ts
@@ -98,7 +98,6 @@ export class MenuComponent implements OnInit {
         },
         {
             label: $localize`Outils`, lvl: 0, displayed: true, authorized: (): boolean => true, expanded: true, children: [
-<<<<<<< HEAD
                 {label: $localize`Camping`, path: 'tools/camping', displayed: true, lvl: 1, authorized: (): boolean => true, spoil: true},
                 {label: $localize`Chances de survie`, path: 'tools/probabilities', displayed: true, lvl: 1, authorized: (): boolean => true, spoil: false},
                 {
@@ -109,10 +108,6 @@ export class MenuComponent implements OnInit {
                     authorized: (): boolean => !environment.production,
                     spoil: false
                 },
-=======
-                { label: $localize`Camping`, path: 'tools/camping', displayed: true, lvl: 1, authorized: (): boolean => true, spoil: false },
-                { label: $localize`Chances de survie`, path: 'tools/probabilities', displayed: true, lvl: 1, authorized: (): boolean => true, spoil: false },
->>>>>>> main
             ], spoil: false
         },
         {
--- MyHordesOptimizerWebsite/src/app/tools/states-management/states-management.component.ts
+++ MyHordesOptimizerWebsite/src/app/tools/states-management/states-management.component.ts
@@ -2,13 +2,13 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggle, MatButtonToggleGroup } from '@angular/material/button-toggle';
+import { MatButtonToggle, MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
-import { MatDivider } from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatList, MatListItem, MatListItemLine } from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import moment from 'moment';
 import { HORDES_IMG_REPO } from '../../_abstract_model/const';
@@ -16,17 +16,18 @@ import { Imports } from '../../_abstract_model/types/_types';
 import { IconApComponent } from '../../shared/elements/icon-ap/icon-ap.component';
 import { IconEpComponent } from '../../shared/elements/icon-ep/icon-ep.component';
 
-const angular_common: Imports = [CommonModule, FormsModule];
-const components: Imports = [];
+const angular_common: Imports = [ CommonModule, FormsModule, NgOptimizedImage ];
+const components: Imports = [ IconEpComponent, IconApComponent ];
 const pipes: Imports = [];
-const material_modules: Imports = [MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatTooltipModule];
+const material_modules: Imports = [ MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatTooltipModule, MatListModule, MatButtonToggleModule, MatDividerModule ];
 
 @Component({
     selector: 'mho-thirst-management',
     templateUrl: './states-management.component.html',
-    styleUrls: ['./states-management.component.scss'],
-    imports: [...angular_common, ...components, ...material_modules, ...pipes, NgOptimizedImage, MatList, MatListItem, IconApComponent, MatDivider, MatListItemLine, MatButtonToggleGroup, MatButtonToggle, IconEpComponent],
-    encapsulation: ViewEncapsulation.None
+    styleUrls: [ './states-management.component.scss' ],
+    encapsulation: ViewEncapsulation.None,
+    host: { style: 'display: contents' },
+    imports: [ ...angular_common, ...components, ...material_modules, ...pipes ],
 })
 export class StatesManagementComponent {
     @HostBinding('style.display') display: string = 'contents';
@@ -35,7 +36,7 @@ export class StatesManagementComponent {
 
     public states_and_actions: (Action | State)[] = [
         { type: 'state', thirst: 'none', ap: 0, wounded: false, ep: 0, cp: 0, shoes: false, bike: false }
-    ]
+    ];
 
     actions: actions[] = [
         { value: 'water', viewValue: 'Prise de gourde' },
@@ -45,8 +46,8 @@ export class StatesManagementComponent {
 
     public addAction(): void {
         this.states_and_actions.push({ type: 'action' });
-        let states = this.states_and_actions.filter((state: StateOrAction) => state.type === 'state')
-        this.states_and_actions.push({ ...states[states.length - 1] });
+        let states = this.states_and_actions.filter((state: StateOrAction) => state.type === 'state');
+        this.states_and_actions.push({ ...states[ states.length - 1 ] });
     }
 
     toggleHydratedStatus($event: MouseEvent, value: string, buttonToggle: MatButtonToggle) {
@@ -70,7 +71,7 @@ interface actions {
 
 
 interface StateOrAction {
-    type: 'state' | 'action'
+    type: 'state' | 'action';
 }
 
 interface State extends StateOrAction {
@@ -81,7 +82,7 @@ interface State extends StateOrAction {
     wounded: boolean;
     cp: number;
     shoes: boolean;
-    bike: boolean
+    bike: boolean;
 }
 
 interface Action extends StateOrAction {
