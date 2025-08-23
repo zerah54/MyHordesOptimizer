46886293da70283cd3d6f7ca73f5754d271f1660
Zerah <helene.godart@telecomnancy.net>
2025-08-23 20:41:35 +0200

    fix radar

--- MyHordesOptimizerApi/MyHordesOptimizerApi/Dtos/MyHordesOptimizer/ExternalsTools/Map/ScoutNextCellsDto.cs
+++ MyHordesOptimizerApi/MyHordesOptimizerApi/Dtos/MyHordesOptimizer/ExternalsTools/Map/ScoutNextCellsDto.cs
@@ -7,18 +7,18 @@ namespace MyHordesOptimizerApi.Dtos.MyHordesOptimizer.ExternalsTools.Map
     {
         [JsonProperty("north")]
         [JsonPropertyName("north")]
-        public int North { get; set; }
+        public int? North { get; set; }
 
         [JsonProperty("south")]
         [JsonPropertyName("south")]
-        public int South { get; set; }
+        public int? South { get; set; }
 
         [JsonProperty("east")]
         [JsonPropertyName("east")]
-        public int East { get; set; }
+        public int? East { get; set; }
 
         [JsonProperty("west")]
         [JsonPropertyName("west")]
-        public int West { get; set; }
+        public int? West { get; set; }
     }
 }
