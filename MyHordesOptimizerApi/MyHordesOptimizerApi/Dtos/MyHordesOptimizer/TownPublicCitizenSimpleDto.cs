925670e2b897fea0bf0642fb8e5b32905db33a9b
Zerah <lenoune38@gmail.com>
2026-07-08 02:49:50 +0200

    [Site] Theming + amélio wislist + fixes

---
+++ MyHordesOptimizerApi/MyHordesOptimizerApi/Dtos/MyHordesOptimizer/TownPublicCitizenSimpleDto.cs
@@ -0,0 +1,9 @@
+namespace MyHordesOptimizerApi.Dtos.MyHordesOptimizer
+{
+    public class TownPublicCitizenSimpleDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? DeathTypeId { get; set; }
+    }
+}
