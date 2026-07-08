7b6ca41cf5da0e94c88df70b0f756aaa42f93a87
Zerah <lenoune38@gmail.com>
2026-07-08 02:45:34 +0200

    [Site] Theming + amélio wislist + fixes

---
+++ MyHordesOptimizerApi/MyHordesOptimizerApi/Dtos/MyHordesOptimizer/TownListItemDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace MyHordesOptimizerApi.Dtos.MyHordesOptimizer
+{
+    public class TownListItemDto
+    {
+        public int Id { get; set; }
+        public int? MapId { get; set; }
+        public string? Name { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+        public TownType? TownType { get; set; }
+        public int? Season { get; set; }
+        public TownPhase? Phase { get; set; }
+        public string? Language { get; set; }
+        public int? Score { get; set; }
+        public int? SoulPoints { get; set; }
+        public bool IsChaos { get; set; }
+        public bool IsDevasted { get; set; }
+        public bool IsFinished { get; set; }
+        public List<TownPublicCitizenSimpleDto> Citizens { get; set; } = new();
+    }
+}
---
+++ MyHordesOptimizerApi/MyHordesOptimizerApi/Dtos/MyHordesOptimizer/TownPhase.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Converters;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace MyHordesOptimizerApi.Dtos.MyHordesOptimizer
+{
+    [Newtonsoft.Json.JsonConverter(typeof(JsonStringEnumConverter))]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TownPhase
+    {
+        // Le nom des membres doit correspondre exactement à la valeur attendue par le front (cf.
+        // _types.ts : TownPhase = 'ALPHA' | 'BETA' | 'IMPORT' | 'NATIVE') : le JsonStringEnumConverter
+        // global (Program.cs) sérialise sur le nom du membre, pas sur [EnumMember]/[Description].
+        [Description("ALPHA")]
+        [EnumMember(Value = "ALPHA")]
+        ALPHA,
+        [Description("BETA")]
+        [EnumMember(Value = "BETA")]
+        BETA,
+        [Description("IMPORT")]
+        [EnumMember(Value = "IMPORT")]
+        IMPORT,
+        [Description("NATIVE")]
+        [EnumMember(Value = "NATIVE")]
+        NATIVE
+    }
+}
